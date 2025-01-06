1ff4de3c91fa8c75167fd233db1aa08b3d53dc1e
leon1010000 <leoniz@outlook.jp>
2025-01-06 14:34:35 +0900

    c

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const VERSION = "v1";
 const CACHE_NAME = `period-tracker-${VERSION}`;
 const APP_STATIC_RESOURCES = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./app.js",
-    "./icon.png",
+    "/",
+    "/index.html",
+    "/style.css",
+    "/app.js",
+    "/icon.png",
 ];
 self.addEventListener("install", (event) => {
   console.log("installed");
